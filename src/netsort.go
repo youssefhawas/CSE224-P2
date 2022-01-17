e63dcd7d2129ca41afd8cd7a864306ae901250c6
Youssef Hawas <youssefhawas@Youssefs-MacBook-Pro.local>
2022-01-16 17:15:25 -0800

    Established mesh and prototype recieve

--- src/netsort.go
+++ src/netsort.go
@@ -2,11 +2,14 @@ package main
 
 import (
 	"fmt"
-	"gopkg.in/yaml.v2"
 	"io/ioutil"
 	"log"
+	"net"
 	"os"
 	"strconv"
+	"time"
+
+	"gopkg.in/yaml.v2"
 )
 
 type ServerConfigs struct {
@@ -30,6 +33,62 @@ func readServerConfigs(configPath string) ServerConfigs {
 	return scs
 }
 
+func handleConnection(conn net.Conn, ch chan<- []byte) {
+	buffer := make([]byte, 100)
+	bytes, err := conn.Read(buffer)
+	if err != nil {
+		log.Panicln(err)
+	}
+	fmt.Print("Received from")
+	fmt.Print(conn)
+	fmt.Println(buffer[0:bytes])
+	ch <- buffer[0:bytes]
+}
+
+func listenforData(ch chan<- []byte, serverid int) {
+	listener, err := net.Listen("tcp", "server"+strconv.Itoa(serverid)+":8080")
+	if err != nil {
+		log.Panic(err)
+	}
+	fmt.Println("Server UP")
+	defer listener.Close()
+
+	time.Sleep(1 * time.Second)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Panic(err)
+		}
+		fmt.Println("Accepted connection from: ")
+		fmt.Println(conn)
+		go handleConnection(conn, ch)
+	}
+}
+
+func sendData(conn net.Conn) {
+	valbytes := []byte(strconv.Itoa(12))
+	conn.Write(valbytes)
+}
+
+func dialToServers(serverId int, scs ServerConfigs) {
+	time.Sleep(1 * time.Second)
+	for _, serv := range scs.Servers {
+		if serv.Host == ("server" + strconv.Itoa(serverId)) {
+			continue
+		} else {
+			for {
+				conn, err := net.Dial("tcp", serv.Host+":"+serv.Port)
+				if err == nil {
+					go sendData(conn)
+					break
+				}
+				defer conn.Close()
+			}
+		}
+	}
+}
+
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
@@ -48,7 +107,13 @@ func main() {
 	scs := readServerConfigs(os.Args[4])
 	fmt.Println("Got the following server configs:", scs)
 
+	ch := make(chan []byte)
+	// records := [][]byte{}
+	print("going to listen")
+	go listenforData(ch, serverId)
+	go dialToServers(serverId, scs)
 	/*
 		Implement Distributed Sort
 	*/
+	time.Sleep(100 * time.Second)
 }
